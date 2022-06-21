bba2c9b277897b9dbd663544c7160f066b9fbc39
rdavis@galileosuite.com <rdavis@galileosuite.com>
2022-06-21 09:02:32 -0700

    samps works

--- samps/main.go
+++ samps/main.go
@@ -3,36 +3,50 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
-
-	"github.com/davecgh/go-spew/spew"
+	"strings"
 )
+
+type Pods struct {
+}
 
 func main() {
 
-	var token string = "c6325da8-65ce-7cd9-8a08-a4e9d0204328"
+	var token string = "af71f0a4-45d8-8be1-4d4c-98536fbfe81f"
+	sessionToken := getSessionToken(token)
 
+	emptyBody, _ := json.Marshal(map[string]string{})
+	podGet, err := http.NewRequest("GET", "https://flasharray1.testdrive.local/api/1.19/pod", bytes.NewBuffer(emptyBody))
+	if err != nil {
+		log.Fatal("cannot GET pod")
+	}
+	podGet.Header.Add("Cookie", sessionToken)
+
+	client := &http.Client{}
+	podData, err := client.Do(podGet)
+	if err != nil {
+		log.Fatal("unable to create http client")
+	}
+	defer podData.Body.Close()
+	pods, err := io.ReadAll(podData.Body)
+	fmt.Println(string(pods))
+}
+
+func getSessionToken(token string) string {
 	requestBody, err := json.Marshal(map[string]string{
 		"api_token": token,
 	})
-
-	emptyBody, err := json.Marshal(map[string]string{})
-
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	resp, err := http.Post("https://flasharray1.testdrive.local/api/1.19/auth/session", "application/json", bytes.NewBuffer(requestBody))
 
 	if err != nil {
 		log.Fatal("cannot load")
 	}
-	cookies := resp.Header["Set-Cookie"]
+	cookies := resp.Header["Set-Cookie"][0]
+	sessionToken := strings.Split(cookies, ";")[0]
 
-	podGet, err := http.NewRequest("GET", "https://flasharray1.testdrive.local/api/1.19/pod", bytes.NewBuffer(emptyBody))
-	podGet.Header.Add("Cookie", cookies[0])
-
-	spew.Dump(podGet)
-
+	return sessionToken
 }
